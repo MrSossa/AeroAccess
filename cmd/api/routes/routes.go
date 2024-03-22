27fb5c03610a7ae96a1f9137b7611e11f6af6518
MrSossa <kasossac@gmail.com>
2024-03-22 13:00:41 -0500

    init

---
+++ cmd/api/routes/routes.go
@@ -0,0 +1,21 @@
+package routes
+
+import (
+	"database/sql"
+
+	"github.com/gin-gonic/gin"
+)
+
+type Router interface {
+	MapRoutes()
+}
+
+type router struct {
+	r  *gin.Engine
+	rg *gin.RouterGroup
+	db *sql.DB
+}
+
+func NewRouter(r *gin.Engine, db *sql.DB) Router {
+	return &router{r: r, db: db}
+}
